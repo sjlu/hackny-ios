6b5479f3d99fc542c734f91832c3e94bae6274ee
Steven Lu <tacticalazn@gmail.com>
2015-02-23 23:52:14 -0500

    updating scheme for hackny

--- MHacks/MHacks/Color.swift
+++ MHacks/MHacks/Color.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct Color {
     
-    static let red    = UIColor(red: 226.0/255.0, green: 048.0/255.0, blue: 082.0/255.0, alpha: 1.0) // #E23052
+    static let red    = UIColor(red: 228.0/255.0, green: 038.0/255.0, blue: 040.0/255.0, alpha: 1.0) // #e42628
     static let orange = UIColor(red: 247.0/255.0, green: 139.0/255.0, blue: 049.0/255.0, alpha: 1.0) // #F78B31
     static let yellow = UIColor(red: 255.0/255.0, green: 202.0/255.0, blue: 011.0/255.0, alpha: 1.0) // #FFCA0B
     static let green  = UIColor(red: 077.0/255.0, green: 153.0/255.0, blue: 115.0/255.0, alpha: 1.0) // #4D9973
